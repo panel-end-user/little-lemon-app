7c18e9f5ac1544376a8dc74bf57ad8fbbad06bde
panel-end-user <sevensegmentdisciple@gmail.com>
2023-05-18 17:44:32 -0600

    Semantic HTML and meta tags.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import Header from './Components/Header.js'
+import Nav from './Components/Nav.js'
+import Main from './Components/Main.js'
+import Footer from './Components/Footer.js'
 
 function App() {
   return (
-    <div className="App">
-      Homepage
-    </div>
+    <>
+      <Header/ >
+      <Nav/ >
+      <Main/ >
+      <Footer/ >
+    </>
   );
 }
 
---
+++ src/Components/Footer.js
@@ -0,0 +1,35 @@
+function Footer() {
+    return (
+        <footer>
+            <img src=""></img>
+            <section>
+                <nav>
+                    <ul>
+                        <li><a href="/home">Home</a></li>
+                        <li><a href="/about">About</a></li>
+                        <li><a href="/menu">Menu</a></li>
+                        <li><a href="/reservations">Reservations</a></li>
+                        <li><a href="/orderonline">Order Online</a></li>
+                        <li><a href="/login">Login</a></li>
+                    </ul>
+                </nav>
+            </section>
+            <section>
+                <ul>
+                    <li>Address</li>
+                    <li>Phone Number</li>
+                    <li>Email</li>
+                </ul>
+            </section>
+            <section>
+                <ul>
+                    <li><a href="/social1">Social 1</a></li>
+                    <li><a href="/social2">Social 2</a></li>
+                    <li><a href="/social3">Social 3</a></li>
+                </ul>
+            </section>
+        </footer>
+    )
+}
+
+export default Footer;
